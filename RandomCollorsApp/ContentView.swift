9a000b6b14e9eba2f887fdd9b3387878174f2ab9
Breno Araripe <brenoma@live.com>
2023-12-08 23:58:30 -0300

    Initial Commit

---
+++ RandomCollorsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomCollorsApp
+//
+//  Created by Breno Araripe on 08/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomCollorsApp/RandomCollorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCollorsAppApp.swift
+//  RandomCollorsApp
+//
+//  Created by Breno Araripe on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomCollorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
